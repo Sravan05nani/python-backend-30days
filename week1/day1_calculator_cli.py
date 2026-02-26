60a7e4b9918968b47a2424444886ce4a2f1e9cd1
Sravan Kumar M V <vaddenani05@gmail.com>
2026-02-26 15:13:38 +0530

    Day-1: Built CLI calculator using variables,input and arithmetic

---
+++ week1/day1_calculator_cli.py
@@ -0,0 +1,21 @@
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+operation = input("Choose your Operation (+, -, *, /):")
+
+if operation == "+":
+    result = num1+num2
+elif operation == "-":
+    result = num1-num2
+elif operation == "*":
+    result = num1*num2
+elif operation == "/":
+    if num2 ==0:
+        result = print("Error: Division by zero is not allowed")
+    else:
+        result = num1/num2
+else:
+    result = "Enter valid operation"
+
+print("Result :", result)
+    
